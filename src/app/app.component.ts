3127542ffd3486eeddf26f78c37d1c4eabbe5da5
KOHETs <kohet.s@gmail.com>
2022-08-27 21:59:09 +0900

    feat: tutorial - toh-pt1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeroesComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
