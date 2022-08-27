7140e1f1e42990ff05fcec87897e2a98a12f3b4f
KOHETs <kohet.s@gmail.com>
2022-08-27 22:04:51 +0900

    feat: tutorial - toh-pt3

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
