7482823f8799b631c3eb4fbbf2c6422666daeac0
KOHETs <kohet.s@gmail.com>
2022-08-27 22:46:20 +0900

    feat: tutorial - toh-pt6

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Hero } from '../hero';
+import { HeroSearchComponent } from '../hero-search/hero-search.component';
 import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HeroSearchComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { enableProdMode } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 import { HeroService } from './app/hero.service';
+import { InMemoryDataService } from './app/in-memory-data.service';
 import { MessageService } from './app/message.service';
 import { environment } from './environments/environment';
 
@@ -12,5 +15,16 @@ if (environment.production) {
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes), HeroService, MessageService],
+  providers: [
+    provideRouter(appRoutes),
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+        dataEncapsulation: false,
+      })
+    ),
+    HeroService,
+    MessageService,
+    InMemoryDataService,
+  ],
 }).catch((err) => console.error(err));
